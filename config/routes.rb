287bb510c194a4f0b246a4046630249f0b2b2b78
kristi smith <kri11smith@gmail.com>
2013-08-20 10:31:31 -0500

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 DtmfApp::Application.routes.draw do
   get "pages/index"
-  get "pages/schedule"
-  get "pages/location"
+  get "/schedule" => 'pages#schedule'
+  get "/location" => 'pages#location'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
